e85a51e4fa7a16a51d0e40b8ca36e28b0ddfcff4
Harsh <harshvj03@gmail.com>
2021-04-24 19:16:36 +0530

    added all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import { useState,useEffect } from 'react';
 import './App.css';
+import Todo from './Todo';
+import db from './firebase';
+import firebase from './firebase';
 
 function App() {
+
+  const [todos, setTodo] = useState([]);
+
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot => {
+      // console.log(snapshot.docs.map(doc => doc.data()));
+      setTodo(snapshot.docs.map(doc => doc.data().todo));
+    })
+  }, [])
+
+  const addTodo = (event) => {
+
+    event.preventDefault();
+    // console.log('👽', 'im working');
+    
+    db.collection('todos').add({
+      todo : input,
+      timestamp : firebase.firestore.FieldValue.serverTimestamp()
+    })
+    // setTodo([...todos, input]);
+    setInput('');
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-do App 🚀</h1>
+      <form>
+        <FormControl>
+          <InputLabel>✅ Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+
+        </FormControl>
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map(todo => <Todo text={todo} />)}
+      </ul>
     </div>
   );
 }
